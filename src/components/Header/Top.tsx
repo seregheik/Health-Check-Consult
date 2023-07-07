a71d769429f9655ec0cd508e99b47b556c980e08
Osasere <edenosas1@gmail.com>
2023-07-07 09:29:48 +0100

    Top part of navbar done

---
+++ src/components/Header/Top.tsx
@@ -0,0 +1,30 @@
+import { Grid, Box, Button, Stack } from "@mui/material";
+import logo from "../../assets/headericons/HCC.png";
+
+const Top = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid height={{ sm: 114, xs: 68 }} container spacing={2}>
+        <Grid height={{ sm: 114, xs: 68 }} display={"flex"}
+          justifyContent={"flex-start"} item xs={6}>
+          <Stack width={{ xs: 130 }} display={'flex'} p={{xs:1, md:3 }} justifyContent={'center'}>
+            <img src={logo} alt="Health Check Consult" />
+          </Stack>
+        </Grid>
+        <Grid
+          height={{ sm: 114, xs: 68 }}
+          item
+          display={"flex"}
+          justifyContent={"flex-end"}
+          xs={6}
+        >
+          <Stack display={"flex"} justifyContent={"center"} p={{xs:1, md:3}}>
+            <Button size="small" variant="contained">Get Started</Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Top;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#BA68C8',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
